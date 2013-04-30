afcba05b644195dcb941b7b53c33bdecbc765904
James Gillmore <james@faceyspacey.com>
2013-04-30 09:49:23 -0700

    stuff just before launch after old site died

--- client/client.js
+++ client/client.js
@@ -1,10 +1,12 @@
 Meteor.Router.add({
 	'/': function() {
 		Session.set('page', 'home');
+		$('html,body').animate({scrollTop: 0}, 200);
 		return 'page_home'
 	},
 	'/:page': function(page) {
 		Session.set('page', page);
+		$('html,body').animate({scrollTop: 0}, 200);
 		return 'page_'+page;
 	},
 });
--- client/template_js~/header.js
+++ client/template_js~/header.js
@@ -5,3 +5,41 @@ Template.header.active_about = function() {
 Template.header.active_contact = function() {
 	return Session.equals('page', 'contact') ? 'active' : '';
 };
+
+var dropdownTimer;
+Template.header.events({
+	'mouseenter .nav_button': function() {
+		$('.dropdown').fadeIn('fast');
+	},
+	'mouseleave .nav_button': function() {
+		dropdownTimer = setTimeout(function() {
+			$('.dropdown').fadeOut('fast');
+		}, 100);
+	},
+	
+	'mouseenter .dropdown': function() {
+		clearTimeout(dropdownTimer);
+		$('.dropdown').fadeIn('fast');
+	},
+	'mouseleave .dropdown': function() {
+		$('.dropdown').fadeOut('fast');
+	},
+	'mouseenter .dropdown li': function(e) {
+		$(e.currentTarget).find('a').addClass('active');
+	},
+	'mouseleave .dropdown li': function(e) {
+		$('.dropdown li a').removeClass('active');
+	},
+	'click .dropdown li': function(e) {
+		$('.nav_button h2').text($(e.currentTarget).text());
+	}
+});
+
+Meteor.startup(function() {
+	$('.logo a').live('click', function() {
+		$('.nav_button h2').text('Surgical Procedures');
+	});
+	$('#nav a').live('click', function() {
+		$('.nav_button h2').text('Surgical Procedures');
+	});
+});
